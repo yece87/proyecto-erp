01fc0db0edaeae6d3eb7405f950cd5062421ee06
yece87 <symundoshermanos@gmail.com>
2024-11-25 21:33:58 -0500

    Update erp.py
    Cambio de logo

--- erp.py
+++ erp.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 from fpdf import FPDF
 
-logo_path = https://www.yeeply.com/wp-content/uploads/2024/04/erp-planificacion-recursos-empresariales-produccion-industrial-productividad-mejora-empresa_100456-8252.jpeg
+logo_path = https://www.crushpixel.com/big-static23/preview4/erp-letter-logo-design-with-6027127.jpg
 # Configuración inicial
 st.set_page_config(page_title="Módulos del ERP", layout="wide",page_icon=logo_path)
 # Ruta del archivo de imagen (logo)
